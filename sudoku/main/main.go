9f9db53edc1d84b945b3c97473b83576a7916dae
ysnbhb <yassine.bahbib@usmba.ac.am>
2024-03-13 20:54:55 +0000

    sudoku

--- sudoku/main/main.go
+++ sudoku/main/main.go
@@ -10,6 +10,7 @@ func main() {
 	arr := os.Args[1:]
 	if len(arr) == 1 {
 		arr = piscine.SplitWithSpace(arr[0])
+		//use split whit space in contoin if (go run . "".96.4...1" "1...6...4" "5.481.39." "..795..43" ".3..8...." "4.5.23.18" ".1.63..59" ".59.7.83." "..359...0"")
 	}
 	if len(arr) != 9 {
 		fmt.Println("Please provide a 9x9 Sudoku puzzle")
