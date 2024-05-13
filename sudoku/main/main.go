d33841620ca98f434a91298b2fb1638c8b87de4b
ybahbib <ybahbib@b2f1r9s2>
2024-05-13 14:07:40 +0100

    so

--- sudoku/main/main.go
+++ sudoku/main/main.go
@@ -3,28 +3,27 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"piscine"
 )
 
 func main() {
-	arr := os.Args[1:]
-	if len(arr) == 1 {
-		arr = piscine.SplitWithSpace(arr[0])
-		//use split whit space in contoin if (go run . "".96.4...1" "1...6...4" "5.481.39." "..795..43" ".3..8...." "4.5.23.18" ".1.63..59" ".59.7.83." "..359...0"")
-	}
-	if len(arr) != 9 {
+	args := os.Args[1:]
+	joinedargs := strings.Join(args, " ")
+	args = strings.Split(joinedargs, " ")
+	if len(args) != 9 {
 		fmt.Println("Please provide a 9x9 Sudoku puzzle")
 		return
 	}
 	var tr [][]int
-	for i := 0; i < len(arr); i++ {
-		if len(arr[i]) != 9 {
+	for i := 0; i < len(args); i++ {
+		if len(args[i]) != 9 {
 			fmt.Println("error")
 			return
 		}
-		tr = append(tr, piscine.Tonumbre(arr[i]))
+		tr = append(tr, piscine.Tonumbre(args[i]))
 	}
-	for i := 0; i < len(arr); i++ {
+	for i := 0; i < len(args); i++ {
 		for j := 0; j < 9; j++ {
 			if tr[i][j] == 0 {
 				continue
